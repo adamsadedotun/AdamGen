f3620b397941300a7205a5910e7f44ebf0b1168d
Adedotun Adams <adamsadedotun95@gmail.com>
2025-01-07 21:41:21 +0300

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 
 setup(
     name='AdamGen',
-    version='0.0.1',
+    version= '0.0.1',
     author='Adedotun Adams',
     author_email='adamsadedotun95@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
